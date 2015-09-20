4cc546c0e27de7941c658c2016db8da02c376975
lenny93 <lcutugno@msn.com>
2015-09-20 01:31:23 -0600

    fixing suggestion errors

--- index.js
+++ index.js
@@ -2,17 +2,31 @@ var {Cc, Ci} = require("chrome");
 
 var gcSvc = Cc["@mozilla.org/grammarcheck;1"].
 				getService(Ci.nsIEditorGrammarCheck);
-				
+var { setTimeout, clearTimeout } = require("sdk/timers");	
+
+var currentText;
+var startPos = [1,4];
+var endPos = [2,9];
+
+function dispatchErrors(text)
+{
+	if(currentText != text)
+		return;
+	
+	gcSvc.errorsFound(startPos, endPos, startPos.length);
+	gcSvc.addSuggestionForError(0, "replaced", "msg1");
+	gcSvc.addSuggestionForError(1, "suggestion1", "msg2");
+	gcSvc.addSuggestionForError(1, "suggestion2", "msg2");
+}
 
 function handleWord(word) 
 {
-	var startPos = [0,4];
-	var endPos = [2,9];
+	currentText = word;
 	
-	gcSvc.errorsFound(startPos, endPos, startPos.length);
-	gcSvc.addSuggestionForError(0, "[this will be replaced]", "desc");
-	gcSvc.addSuggestionForError(0, "[this will be replaced2]", "desc2");
-	gcSvc.addSuggestionForError(1, "[another suggestion]", "desc");
+	console.error("Handle Word");
+	
+
+	setTimeout(dispatchErrors(word), 5000)
 }
 
 				
